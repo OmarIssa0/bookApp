3b8d297b829c2e6ad0b14a2bf6f86fde22d80bd8
OmarIssa0 <omar.issa.slwade@gmail.com>
2023-04-28 20:20:20 +0300

    create splash body view

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'features/Splash/presentation/views/splash_view.dart';
+
+void main() {
+  runApp(
+    const BooklyApp(),
+  );
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      debugShowCheckedModeBanner: false,
+      home: const SplashView(),
+    );
+  }
+}
