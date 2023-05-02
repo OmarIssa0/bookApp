aa91017b02d0417e203b349435819e653cf58773
OmarIssa0 <omar.issa.slwade@gmail.com>
2023-05-02 21:16:30 +0300

    search view done

--- lib/features/search/presentaion/views/widgets/search_view_body.dart
+++ lib/features/search/presentaion/views/widgets/search_view_body.dart
@@ -1,3 +1,5 @@
+import 'package:book_app/core/utils/styles.dart';
+import 'package:book_app/features/home/presentation/views/widgets/book_list_view_item.dart';
 import 'package:flutter/material.dart';
 import 'custom_search_text_field.dart';
 
@@ -9,11 +11,42 @@ class SearchViewBody extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: const [
           CustomSearchTextField(),
+          SizedBox(
+            height: 16,
+          ),
+          Text(
+            'Search Result',
+            style: Styles.textStyle18,
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Expanded(
+            child: SearchResultListView(),
+          ),
         ],
       ),
     );
   }
 }
 
+class SearchResultListView extends StatelessWidget {
+  const SearchResultListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: EdgeInsets.zero,
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return const Padding(
+          padding: EdgeInsets.symmetric(vertical: 10),
+          child: BookListViewItem(),
+        );
+      },
+    );
+  }
+}
