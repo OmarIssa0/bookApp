714f15e7d8386c5fdaf7c52e9fa50c6205bd7426
OmarIssa0 <omar.issa.slwade@gmail.com>
2023-05-03 00:18:48 +0300

    create Server failure form dio error

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -3,6 +3,7 @@ import 'package:book_app/features/home/data/models/book_model/book_model.dart';
 import 'package:book_app/core/errors/failures.dart';
 import 'package:book_app/features/home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl implements HomeRepo {
   final ApiService apiService;
@@ -22,7 +23,17 @@ class HomeRepoImpl implements HomeRepo {
       }
       return right(books);
     } catch (e) {
-      return left(ServerFailure());
+      if (e is DioError) {
+        return left(
+          ServerFailure.fromDioError(e),
+        );
+      }
+
+      return left(
+        ServerFailure(
+          e.toString(),
+        ),
+      );
     }
   }
 
